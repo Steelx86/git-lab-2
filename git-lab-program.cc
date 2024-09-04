62a1cd53edff856aeca03b329f69445bdcd7139f
Steelx86 <74436628+Steelx86@users.noreply.github.com>
2024-09-04 17:49:06 -0400

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: John Steel
+ *        Date: September 4th, 2024
+ * Description: Greets the program git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
